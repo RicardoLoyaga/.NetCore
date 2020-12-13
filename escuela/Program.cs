be43c7ddd595017407ef9c7005065cc984255927
Bryan Armas <bral_9210@hotmail.com>
2020-12-13 00:13:25 -0500

    Creacion del proyecto escuela

---
+++ escuela/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace escuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int anioFundacion;
+        public string ceo; // = "Bryan Armas";
+
+        public void Timbrar(){
+            //todo
+            Console.Beep(10000,3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Escuela miEscuela = new Escuela();
+            miEscuela.nombre = "Bryan School";
+            miEscuela.direccion = "Quito-Ecuador";
+            miEscuela.anioFundacion = 2020;
+            Console.WriteLine("Timbrando mi escuela");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
